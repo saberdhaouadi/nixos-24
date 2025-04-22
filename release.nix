36162a10c800e3bb77f6d383110b934a8528e352
saber.dhaouadi <saber.dhaouadi@infor.com>
2025-04-22 23:15:52 +0100

    changed the content of release.nix

--- release.nix
+++ release.nix
@@ -4,17 +4,15 @@ let
   pkgs = import nixpkgs { system = "x86_64-linux"; };
   nixos = import "${nixpkgs}/nixos";
   configuration = {
-    # Minimal EC2 configuration
     imports = [
       "${nixpkgs}/nixos/maintainers/scripts/ec2/amazon-image.nix"
     ];
+    ec2.hvm = true; # Ensure HVM virtualization for EC2
    # nixpkgs.hostPlatform = "x86_64-linux";
     # Optional: Add custom configurations
     services.sshd.enable = true;
     environment.systemPackages = with pkgs; [ git vim ];
- };
+  };
 in {
   amazonImage = (nixos { configuration = configuration; }).config.system.build.amazonImage;
 }
-
-
